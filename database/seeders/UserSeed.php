3729ab1abd6ef202c5f7c54dc8d1a2b942da53d5
Jefferson Silva <jeffersonsantsil@gmail.com>
2022-07-07 22:21:31 -0300

    WIP

---
+++ database/seeders/UserSeed.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::created([
+            'name' => 'Jefferson Silva',
+            'email' => 'jeffersonsantsil@gmail.com',
+            'password' => bcrypt(123456)
+        ]);
+    }
+}
